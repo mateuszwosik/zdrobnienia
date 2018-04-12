a2af589ec27a3aaeb96d1ba8de98d21486b0be66
mateuszwosik <mateusz.wosik@gmail.com>
2018-04-12 11:39:50 +0200

    ADD send report mail

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, request
+from flask_mail import Mail, Message
 from diminutivesFinder import *
 
 app = Flask(__name__)
+mail = Mail(app)
+
+app.config.update(
+    #EMAIL SETTINGS
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = 465,
+    MAIL_USE_SSL = True,
+    MAIL_USERNAME = 'wyszukiwarkazdrobnien@gmail.com',
+    MAIL_PASSWORD = '')
+
+mail = Mail(app)
 
 @app.route('/')
 def index():
@@ -19,8 +31,15 @@ def result():
 def about():
     return render_template('about.html')
 
-@app.route('/report')
+@app.route('/report', methods=['GET','POST'])
 def report():
+    if request.method == "POST":
+        #sending mail
+        msg = Message('REPORT - zgłoszono słowo',
+                      sender = 'wyszukiwarkazdrobnien@gmail.com',
+                      recipients = ['wyszukiwarkazdrobnien@gmail.com'])
+        msg.body = "SŁOWO: " + request.form['word'] + " \r\nKOMENTARZ: " + request.form['comment']
+        mail.send(msg)        
     return render_template('report.html')
 
 if __name__ == '__main__':
